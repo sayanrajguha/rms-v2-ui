497eea243ea6f91f8da7e8361dd5e3bb55e780c8
Sayanraj Guha <sayanrajguha@gmail.com>
2017-04-28 08:27:24 +0000

    project file structure created. Modules categorized. Static resources added.

--- public/global-scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-/************************
-Global Script file
-Author : Sayanraj Guha
-fileName : scripts.js
-Description : Contains all the custom scripts applicable to RMS Portal for P.E.Erectors Pvt. Ltd. upgrade v2.0
-All rights reserved.
-************************/
